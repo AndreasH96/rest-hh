4e399aa88fe8dbfec68f4415d8a76c1360d982b0
Emil Pirfält <emil.pirfalt@gmail.com>
2021-11-24 21:20:14 +0100

    Move food js function into food file

--- docs/script.js
+++ docs/script.js
@@ -1,27 +1,3 @@
-// APIs
-
-export async function food({ output, foodType = "burger", width = 300 } = {}) {
-  // Random food
-  const responseRandom = await fetch("https://foodish-api.herokuapp.com/api/");
-  const dataRandom = await responseRandom.json();
-  logJSON(dataRandom);
-  const randomImg = document.createElement("img");
-  randomImg.src = dataRandom.image;
-  randomImg.width = width;
-  output.appendChild(randomImg);
-
-  // Typed random food
-  const responseTyped = await fetch(
-    `https://foodish-api.herokuapp.com/api/images/${foodType}`
-  );
-  const dataTyped = await responseTyped.json();
-  logJSON(dataTyped);
-  const idlyTyped = document.createElement("img");
-  idlyTyped.src = dataTyped.image;
-  idlyTyped.width = width;
-  output.appendChild(idlyTyped);
-}
-
 // Helpers
 export function logJSON(v) {
   console.log(JSON.stringify(v, null, 2));
