6f47d19816b97be6678b7983e807211315ee7fb2
Emil Pirfält <emil.pirfalt@gmail.com>
2021-11-24 21:08:05 +0100

    Move quiz js function into quiz file

--- docs/script.js
+++ docs/script.js
@@ -1,50 +1,5 @@
 // APIs
 
-export async function quiz({ output, template }) {
-  const computerScienceURL =
-    "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=boolean&encode=url3986";
-  // const scienceAndNatureURL =
-  //   "https://opentdb.com/api.php?amount=10&category=17&difficulty=medium&type=boolean&encode=url3986";
-  const response = await fetch(computerScienceURL);
-
-  const data = await response.json();
-  logJSON(data);
-
-  // data.results
-  // [
-  //   {
-  //     "category": "Science: Computers",
-  //     "type": "boolean",
-  //     "difficulty": "medium",
-  //     "question": "The common software-programming acronym &quot;I18N&quot; comes from the term &quot;Interlocalization&quot;.",
-  //     "correct_answer": "False",
-  //     "incorrect_answers": [
-  //       "True"
-  //     ]
-  //   }...
-  // ]
-  data.results
-    .map((r) => {
-      const q = template.content.cloneNode(true);
-      const f = q.querySelector("[data-name=question_form]");
-      const p = q.querySelector("[data-name=question_p]");
-      const o = q.querySelector("[data-name=question_output]");
-      const question = decodeURIComponent(r.question);
-      const correctAnswer = r.correct_answer;
-
-      f.addEventListener("submit", (e) => {
-        e.preventDefault();
-        o.value = e.target.question?.value == correctAnswer;
-      });
-
-      p.textContent = question;
-      return q;
-    })
-    .forEach((q) => {
-      output.appendChild(q);
-    });
-}
-
 export async function food({ output, foodType = "burger", width = 300 } = {}) {
   // Random food
   const responseRandom = await fetch("https://foodish-api.herokuapp.com/api/");
